412c20226dfe57936340a804cbbff52d6493abd8
apimpnamedodogwu <edenelenwoke@gmail.com>
2023-02-28 22:33:28 +0100

    mvc config

--- src/main/java/com/example/customerprofilemanagementsystem/CustomerProfileManagementSystemApplication.java
+++ src/main/java/com/example/customerprofilemanagementsystem/CustomerProfileManagementSystemApplication.java
@@ -13,12 +13,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CustomerProfileManagementSystemApplication {
 
     @Bean
-    public WebMvcConfigurer corsConfigurer(){
+    public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*");
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
             }
         };
     }
